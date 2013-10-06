01451602eb9572a49cfd2f6af62f89fcc9d2eeae
Tiziano Perez Stolfa <tperezstolfa@gmail.com>
2013-10-06 20:54:28 -0300

    Comente la isla y puse un suelo plano por probar
    tambien active la camara de fps para poder moverte por el espacio

--- Random/EjemploAlumno.cs
+++ Random/EjemploAlumno.cs
@@ -10,13 +10,15 @@ using TgcViewer.Utils.Modifiers;
 using TgcViewer.Utils.Terrain;
 using TgcViewer.Utils.TgcSceneLoader;
 using TgcViewer.Utils._2D;
+using TgcViewer.Utils.TgcGeometry;
 
 namespace AlumnoEjemplos.Random
 {
     public class EjemploAlumno : TgcExample
     {
         TgcSkyBox skyBox;
-        TgcScene scene;
+        //TgcScene scene;
+        TgcBox suelo;
         TgcText2d textoCamara;
 
 
@@ -35,6 +37,7 @@ namespace AlumnoEjemplos.Random
             return "Deformaciones - Una descripcion";
         }
 
+
         /// Método que se llama una sola vez,  al principio cuando se ejecuta el ejemplo.
         /// Escribir aquí todo el código de inicialización: cargar modelos, texturas, modifiers, uservars, etc.
         /// Borrar todo lo que no haga falta
@@ -51,9 +54,13 @@ namespace AlumnoEjemplos.Random
             //Crear SkyBox
             createSkyBox(alumnoMediaFolder);
 
+            //suelo
+            TgcTexture pisoTexture = TgcTexture.createTexture(d3dDevice, alumnoMediaFolder + "Random\\Textures\\Terrain\\tileable_grass.jpg");
+            suelo = TgcBox.fromSize(new Vector3(500, 0, 500), new Vector3(7000, 0, 7000), pisoTexture);
+
             //Cargar escenario de Isla
             TgcSceneLoader loader = new TgcSceneLoader();
-            scene = loader.loadSceneFromFile(GuiController.Instance.ExamplesDir + "Optimizacion\\Isla\\Isla-TgcScene.xml");
+            //scene = loader.loadSceneFromFile(GuiController.Instance.ExamplesDir + "Optimizacion\\Isla\\Isla-TgcScene.xml");
 
             //Creo texto para mostrar datos de camara
             textoCamara = new TgcText2d();
@@ -83,23 +90,24 @@ namespace AlumnoEjemplos.Random
             GuiController.Instance.Modifiers.addVertex3f("valorVertice", new Vector3(-100, -100, -100), new Vector3(50, 50, 50), new Vector3(0, 0, 0));
 
 
-
+            /*
             ///////////////CONFIGURAR CAMARA ROTACIONAL//////////////////
             //Es la camara que viene por default, asi que no hace falta hacerlo siempre
             GuiController.Instance.RotCamera.Enable = true;
             //Configurar centro al que se mira y distancia desde la que se mira
             GuiController.Instance.RotCamera.setCamera(new Vector3(40, 650, 2200), 500);
-
-
-            /*
+            */
+   
             ///////////////CONFIGURAR CAMARA PRIMERA PERSONA//////////////////
             //Camara en primera persona, tipo videojuego FPS
             //Solo puede haber una camara habilitada a la vez. Al habilitar la camara FPS se deshabilita la camara rotacional
             //Por default la camara FPS viene desactivada
             GuiController.Instance.FpsCamera.Enable = true;
+            GuiController.Instance.FpsCamera.MovementSpeed = 400;
+            GuiController.Instance.FpsCamera.JumpSpeed = 400;
             //Configurar posicion y hacia donde se mira
-            GuiController.Instance.FpsCamera.setCamera(new Vector3(0, 0, -20), new Vector3(0, 0, 0));
-            */
+            GuiController.Instance.FpsCamera.setCamera(new Vector3(61.8657f, 403.7024f, -527.558f), new Vector3(379.7143f, 12.9713f, 336.3295f));
+
         }
 
 
@@ -115,8 +123,8 @@ namespace AlumnoEjemplos.Random
             textoCamara.Text = GuiController.Instance.CurrentCamera.getPosition().ToString();
             textoCamara.render();
             skyBox.render();
-            scene.renderAll();
-
+            //scene.renderAll();
+            suelo.render();
             
 
             //Obtener valor de UserVar (hay que castear)
@@ -146,14 +154,27 @@ namespace AlumnoEjemplos.Random
 
         }
 
+       
         /// Método que se llama cuando termina la ejecución del ejemplo.
         /// Hacer dispose() de todos los objetos creados.        
         public override void close()
         {
             skyBox.dispose();
-            scene.disposeAll();
+            //scene.disposeAll();
+            suelo.dispose();
         }
-        
+       
+ 
+
+
+
+
+
+
+
+
+
+
         private void createSkyBox(string alumnoMediaFolder)
         {
             //Crear SkyBox 
